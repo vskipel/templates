a9b9c3c1c8aeb8b5602b7cb08d7280f6ab0c28f3
Anton Khomchenko <khomchenko.anton@yandex.ru>
2020-10-20 13:26:48 +0300

    feat: add copy remove

--- index.js
+++ index.js
@@ -22,17 +22,56 @@ const input = document.querySelector('.input');
 const template = document.querySelector('.template');
 
 
-
 const renderList = () => {
+  // методом map разбиваем массив на строки
   const items = todoList.map(element => getItems(element));
+  // добавляем новые строки в конец
   listSection.append(...items);
-}
+};
 
+const handlerDelete = (event) => {
+  // выбираем ближайший родительский элемент к элементу, на котором произошел клик
+  // удаляем методом remove()
+  event.target.closest('.card').remove()
+};
 
+const handlerDuplicate = (event) => {
+  // выбираем ближайший родительский узел в ДОМ
+  console.log(event.target.closest('.card').querySelector('.card__title'));
+  // в нем выбираем заголовок 
+  // и извлекаем его содержимое через innerHTML
+  const cardTitle = event.target.closest('.card').querySelector('.card__title').innerHTML;
+  // передаем значение в функцию обработки карточек
+  const item = getItems({
+    // передаем заголовок как элемент массива
+    title: cardTitle
+  });
+  // вставляем новый элемент в список
+  listSection.prepend(item);
+};
+
+// функция добавления карточек
 const getItems = (data) => {
+  // копируем темплейт
   const card = template.content.cloneNode(true);
-  console.log(card);
+
+  // вставляем в заголовок темплейта значения из массива
   card.querySelector('.card__title').innerText = data.title;
+
+  // объявляем кнопки внутри функции (снаружи не получится)
+  const duplicateBtn = card.querySelector('.duplicate');
+  const deleteBtn = card.querySelector('.delete');
+  
+  // вешаем слушатели на кнопки
+  duplicateBtn.addEventListener('click', handlerDuplicate);
+  deleteBtn.addEventListener('click', handlerDelete);
+
+  //dublicateBtn.addEventListener('click', (evt) => {
+   // evt.cloneNode(true);
+  //})
+  // console.log({dublicateBtn, deleteBtn})
+  
+  
   return card;
 };
 
